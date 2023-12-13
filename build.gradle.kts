c0bf4e05ccebace486c8eb2b72bd468b1239f847
thegr8manef <jedidimanef@gmail.com>
2023-12-13 15:32:08 +0100

    add id("maven-publish")

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("7.4.1").apply(false)
     kotlin("multiplatform").version("1.8.0").apply(false)
+    id("maven-publish")
 }
 
 tasks.register("clean", Delete::class) {
